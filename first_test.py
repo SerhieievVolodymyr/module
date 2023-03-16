5958135f6308e4a68504f47bba7a1625b7392f35
SerhieievVolodymyr <vssergieiev@gmail.com>
2023-03-16 19:47:42 +0200

    Module

---
+++ first_test.py
@@ -0,0 +1,73 @@
+def spam(number):
+    '''Function should return something like this:
+    spam(1);#bulochka
+    spam(3);#bulochkabulochkabulochka
+    But it is broken. Fix it please!
+
+    Эта функция принимает числовой параметр. Должна вернуть строку, которая
+    повторяется столько раз, сколько параметров передано. Она уже написана,
+    но не работает. Любым способом заставьте ее работать.
+    '''
+    return ''.join(['bulochka' for i in range(number)])
+
+
+def my_sum(list_of_numbers):
+
+    """Function receives a list with integer numbers,
+    should return its sum as an integer. Do not use built in summarize functions.
+    :param list
+
+    Функция получает на вход массив чисел, должна вернуть сумму этих чисел.
+    Не использовать встроенные функции суммирования.
+    
+    """
+    s = 0
+    for i in list_of_numbers:
+        s += i
+    return s
+
+
+def shortener(string):
+    """
+    Function receives a long string with many words.
+    It should return the same string, but words,
+    larger then 6 symbols should be changed, symbols
+    after the sixth one should be replaced by symbol *
+    :param string
+    :returns string
+
+     Функция получает на вход длинную строку с множеством слов.
+     Она должна вернуть ту же строку, но в словах, которые длиннее 6 символов,
+     функция должна вместо всех символов после шестого поставить одну звездочку.
+     Пример: Из слова 'verwijdering' должно получиться 'verwij*'
+
+
+    """
+    lis1 = string.split()
+    print(lis1)
+    lis2 = []
+    for word in lis1:
+        if len(word) > 6:
+            lis2.append(word[:6] + '*')
+        else:
+            lis2.append(word)
+    return ' '.join(lis2)
+
+
+def compare_ends(words):
+    """
+    Function receives an array of strings.
+    Please return number of strings, which
+    length is at least 2 symbols and first character
+    is equal to the last character
+
+    Функция получает на вход массив строк. Вернуть надо количество строк,
+    которые не короче двух символов и у которых первый и последний
+    символ совпадают.
+
+    """
+    s = 0
+    for i in words:
+        if len(i) > 1 and i[0] == i[-1]:
+            s += 1
+    return s
